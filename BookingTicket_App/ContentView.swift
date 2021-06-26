9bc8f07a23233f93704bd054f76774b84ca29fc5
ChauWei200220 <65481625+ChauWei200220@users.noreply.github.com>
2021-06-26 17:51:28 +0700

    update

--- BookingTicket_App/ContentView.swift
+++ BookingTicket_App/ContentView.swift
@@ -7,9 +7,11 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct ContentView: View {
     var body: some View {
+        //Home()
         Lobby().preferredColorScheme(.dark)
     }
 }
@@ -22,31 +24,43 @@ struct ContentView_Previews: PreviewProvider {
 
 struct Home: View{
     var body: some View{
-        VStack(spacing: 15){
-            HStack{
-                VStack(alignment: .leading, spacing: 15){
-                    Text("Browse").font(.largeTitle)
-                    Button(action: {
-                        
-                    }) {
-                        HStack(spacing: 8){
-                            Text("Movies In New York")
-                            Image(systemName: "chevron.down").font(.body)
+                VStack(spacing: 15){
+                    HStack{
+                        VStack(alignment: .leading, spacing: 15){
+                            Text("Browse").font(.largeTitle)
+                            Button(action: {
+                                
+                            }) {
+                                HStack(spacing: 8){
+                                    Text("Movies In New York")
+                                    Image(systemName: "chevron.down").font(.body)
+                                }
+                            }.foregroundColor(.black)
                         }
-                    }.foregroundColor(.black)
-                }
-                Spacer()
-                Button(action: {
+                        Spacer()
+                        Button(action: {
+                            
+                        }){
+                            Image("icons8-menu-48").renderingMode(.original)
+                        }
+                    }
+                    SearchView().padding(.vertical,15)
                     
-                }){
-                    Image("icons8-menu-48").renderingMode(.original)
+                    Button(action: {
+                        try! Auth.auth().signOut()
+                        UserDefaults.standard.set(false, forKey: "status")
+                        NotificationCenter.default.post(name: NSNotification.Name("status"), object: nil)
+                    }){
+                        Text("Log out").foregroundColor(.white).fontWeight(.bold).padding(.vertical).padding(.horizontal, 50).background(Color("Color-1")).clipShape(Capsule()).shadow(color: Color.white.opacity(0.1), radius: 5, x: 0, y: 5)
+                    }.background(Color("Color-2")).cornerRadius(10).padding(.top, 35)
+                    
+                    
                 }
+
             }
-            SearchView().padding(.vertical,15)
-            
-        }.padding()
-    }
-}
+        }
+    
+
 
 struct SearchView : View {
     @State var txt = ""
@@ -54,28 +68,44 @@ struct SearchView : View {
         HStack(spacing: 15){
             Image(systemName: "magnifyingglass").font(.body)
             TextField("Search Movies", text: $txt)
-            }.padding().foregroundColor(.black).background(Color("Color"))
+        }.padding().foregroundColor(.black).background(Color.white)
     }
 }
 
 struct Lobby : View {
-    
+    @State var status = UserDefaults.standard.value(forKey: "status") as? Bool ?? false
     @State var index = 0
     
     var body: some View{
-        GeometryReader{_ in
+        NavigationView{
             VStack{
-                Image("icons8-movie-ticket-64").resizable().frame(width: 60, height: 60)
-                
-                ZStack{
-                    SignUp(index: self.$index).zIndex(Double(self.index))
-                    Login(index: self.$index)
+                if self.status{
+                    Home()
                 }
-                
+                else{
+                    GeometryReader{_ in
+                        VStack{
+                            Image("icons8-movie-ticket-64").resizable().frame(width: 60, height: 60)
+                            
+                            ZStack{
+                                SignUp(index: self.$index).zIndex(Double(self.index))
+                                Login(index: self.$index)
+                            }
+                            
+                        }
+                    }.background(Color("Color").edgesIgnoringSafeArea(.all))
+                }
+            }.padding().background(Color("Color").edgesIgnoringSafeArea(.all)).animation(.spring()).onAppear{
+                NotificationCenter.default.addObserver(forName: NSNotification.Name("status"), object: nil, queue: .main) {(_) in
+                    let status = UserDefaults.standard.value(forKey: "status") as? Bool ?? false
+                    self.status = status
+                }
             }
-        }.background(Color("Color").edgesIgnoringSafeArea(.all))
+
+        }
 }
 }
+
 
 struct CShape: Shape {
     func path(in rect: CGRect) -> Path {
@@ -106,55 +136,84 @@ struct Login: View{
     @State var email = ""
     @State var pass = ""
     @Binding var index : Int
+    @State var alert = false
+    @State var error = ""
     
     var body: some View{
-        ZStack(alignment: .bottom){
-            VStack{
-                HStack{
-                    VStack(spacing: 10){
-                        Text("Login").foregroundColor(self.index == 0 ? .white : .gray).font(.title).fontWeight(.bold)
-                        Capsule().fill(self.index == 0 ? Color.blue : Color.clear).frame(width: 100, height: 5)
-                    }
-                    Spacer(minLength: 0)
-                }.padding(.top,40)
-                
+        ZStack{
+            ZStack(alignment: .bottom){
                 VStack{
-                    HStack(spacing: 15){
-                        Image(systemName: "envelope.fill").foregroundColor(Color("Color-1"))
-                        TextField("email Address", text: self.$email)
-                    }
+                    HStack{
+                        VStack(spacing: 10){
+                            Text("Login").foregroundColor(self.index == 0 ? .white : .gray).font(.title).fontWeight(.bold)
+                            Capsule().fill(self.index == 0 ? Color.blue : Color.clear).frame(width: 100, height: 5)
+                        }
+                        Spacer(minLength: 0)
+                    }.padding(.top,40)
                     
-                    Divider().background(Color.white.opacity(0.5))
-                }.padding(.horizontal).padding(.top,40)
-                
-                VStack{
-                    HStack(spacing: 15){
-                        Image(systemName: "eye.slash.fill").foregroundColor(Color("Color-1"))
-                        SecureField("Password", text: self.$pass)
-                    }
-                    
-                    Divider().background(Color.white.opacity(0.5))
-                }.padding(.horizontal).padding(.top,30)
-                
-                HStack{
-                    Spacer(minLength: 0)
-                    Button(action: {
+                    VStack{
+                        HStack(spacing: 15){
+                            Image(systemName: "envelope.fill").foregroundColor(Color("Color-1"))
+                            TextField("email Address", text: self.$email)
+                        }
                         
-                    }){
-                        Text("forget Password?").foregroundColor(Color.white.opacity(0.6))
+                        Divider().background(Color.white.opacity(0.5))
+                    }.padding(.horizontal).padding(.top,40)
+                    
+                    VStack{
+                        HStack(spacing: 15){
+                            Image(systemName: "eye.slash.fill").foregroundColor(Color("Color-1"))
+                            SecureField("Password", text: self.$pass)
+                        }
+                        
+                        Divider().background(Color.white.opacity(0.5))
+                    }.padding(.horizontal).padding(.top,30)
+                    
+                    HStack{
+                        Spacer(minLength: 0)
+                        Button(action: {
+                            
+                        }){
+                            Text("forget Password?").foregroundColor(Color.white.opacity(0.6))
+                        }
                     }
+                    
+                }.padding().padding(.bottom, 65).background(Color("Color-2")).clipShape(CShape()).contentShape(CShape()).shadow(color: Color.black.opacity(0.3), radius: 5, x: 0, y: -5).onTapGesture {
+                    self.index = 0
+                }.cornerRadius(35).padding(.horizontal,20)
+                
+                Button(action: {
+                    self.verify()
+                }){
+                    Text("Login").foregroundColor(.white).fontWeight(.bold).padding(.vertical).padding(.horizontal, 50).background(Color("Color-1")).clipShape(Capsule()).shadow(color: Color.white.opacity(0.1), radius: 5, x: 0, y: 5)
+                }.offset(y:25).opacity(self.index == 0 ? 1 : 0)
+                
+                
+                
+            }
+            if self.alert{
+                ErrorView(alert: self.$alert, error: self.$error)
+            }
+        }
+    }
+    
+    func verify(){
+        if self.email != "" && self.pass != ""{
+            Auth.auth().signIn(withEmail: self.email, password: self.pass) {
+                (res, err) in
+                if err != nil{
+                    self.error = err!.localizedDescription
+                    self.alert.toggle()
+                    return
                 }
-                
-            }.padding().padding(.bottom, 65).background(Color("Color-2")).clipShape(CShape()).contentShape(CShape()).shadow(color: Color.black.opacity(0.3), radius: 5, x: 0, y: -5).onTapGesture {
-                self.index = 0
-            }.cornerRadius(35).padding(.horizontal,20)
-            
-            Button(action: {
-                
-            }){
-                Text("Login").foregroundColor(.white).fontWeight(.bold).padding(.vertical).padding(.horizontal, 50).background(Color("Color-1")).clipShape(Capsule()).shadow(color: Color.white.opacity(0.1), radius: 5, x: 0, y: 5)
-            }.offset(y:25).opacity(self.index == 0 ? 1 : 0)
-            
+                print("success")
+                UserDefaults.standard.set(true, forKey: "status")
+                NotificationCenter.default.post(name: NSNotification.Name("status"), object: nil)
+            }
+        }
+        else{
+            self.error = "Please fill all the contents properly"
+            self.alert.toggle()
         }
     }
 }
@@ -164,14 +223,16 @@ struct SignUp: View{
     @State var pass = ""
     @State var Repass = ""
     @Binding var index : Int
+    @State var alert = false
+    @State var error = ""
     
     var body: some View{
-        ZStack(alignment: .bottom){
+        ZStack{
+            ZStack(alignment: .bottom){
             VStack{
                 HStack{
                     Spacer(minLength: 0)
                     VStack(spacing: 10){
-                        
                         Text("SignUp").foregroundColor(self.index == 1 ? .white : .gray).font(.title).fontWeight(.bold)
                         Capsule().fill(self.index == 1 ? Color.blue : Color.clear).frame(width: 100, height: 5)
                     }
@@ -209,11 +270,71 @@ struct SignUp: View{
             }.cornerRadius(35).padding(.horizontal,20)
             
             Button(action: {
-                
+                self.register()
             }){
                 Text("Sign Up").foregroundColor(.white).fontWeight(.bold).padding(.vertical).padding(.horizontal, 50).background(Color("Color-1")).clipShape(Capsule()).shadow(color: Color.white.opacity(0.1), radius: 5, x: 0, y: 5)
             }.offset(y:25).opacity(self.index == 1 ? 1 : 0)
             
         }
+            
+            if self.alert{
+                ErrorView(alert: self.$alert, error: self.$error)
+            }
+        }
+    }
+    
+    func register(){
+        if self.email != "" {
+            if self.pass == self.Repass{
+                
+                Auth.auth().createUser(withEmail: self.email, password: self.pass){
+                    (res, err) in
+                    if err != nil{
+                        self.error = err!.localizedDescription
+                        self.alert.toggle()
+                    }
+                    print("success")
+                    UserDefaults.standard.set(true, forKey: "status")
+                    NotificationCenter.default.post(name: NSNotification.Name("status"), object: nil)
+                }
+            }
+            else{
+                self.error = "Password mismatch"
+                self.alert.toggle()
+                return
+            }
+        }
+        else{
+            self.error = "Please fill all the contents properly"
+            self.alert.toggle()
+        }
+    }
+}
+
+struct ErrorView : View {
+    @State var color = Color.black.opacity(0.7)
+    @Binding var alert : Bool
+    @Binding var error : String
+    
+    var body: some View{
+        GeometryReader{_ in
+            VStack{
+                HStack{
+                    Text("Error").font(.title).fontWeight(.bold).foregroundColor(self.color)
+                    
+                    Spacer()
+                }.padding(.horizontal, 25)
+                
+                Text(self.error).foregroundColor(.black).padding(.top).padding(.horizontal, 25)
+                
+                Button(action: {
+                    self.alert.toggle()
+                }){
+                    Text("Cancel").foregroundColor(.white).padding(.vertical).frame(width: UIScreen.main.bounds.width - 120)
+                }.background(Color("Color")).cornerRadius(10).padding(.top, 25)
+                
+                
+            }.padding(.vertical, 25).frame(width: UIScreen.main.bounds.width - 70).background(Color.white).cornerRadius(15)
+        }.background(Color.black.opacity(0.35).edgesIgnoringSafeArea(.all))
     }
 }
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'BookingTicket_App' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+pod 'Firebase/Auth'
+
+  # Pods for BookingTicket_App
+
+  target 'BookingTicket_AppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'BookingTicket_AppUITests' do
+    # Pods for testing
+  end
+
+end
